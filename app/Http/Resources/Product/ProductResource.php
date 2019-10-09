b8f68be46a31886bb2e8105f99ffa0e4d96558cc
Sodiq Abdullaev <sodiqabdullaev@mail.ru>
2019-10-09 12:44:43 +0100

    Modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,12 @@ class ProductResource extends Resource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'stock'=>$this->stock == 0 ? 'Out of stock' : $this->stock ,
+            'totalPrice'=>round((1-($this->discount)/100)*$this->discount,2),
+            'rating'=>$this->reviews->count() ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'href'=>[
+                'reviews'=> route('reviews.index', $this->id)
+            ]
         ];
     }
 }
